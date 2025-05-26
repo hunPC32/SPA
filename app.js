1df5a2f6078e0c7651326fd36c53b3cdf40c1ecc
SigMazsola <catfishzsolti@gmail.com>
2025-05-26 09:40:16 +0200

    tspmo🥀

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const { default: firebase } = require("firebase/compat/app");
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 function randomFromArray(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
@@ -6,3 +11,41 @@ function getKeyString(x, y){
     return `(${x}, ${y})`;
 }
 
+(function (){
+
+  let playerId;
+  let playerRef;
+
+  function initGame(){
+    const allPlayersRef = firebase.database().ref(`players`);
+    const allCoinsRef = firebase.database().ref(`coins`);
+  }
+
+  firebase.auth().onAuthStateChanged((user) => {
+    console.log(user);
+    if (user){
+      playerId = user.uid;
+      playerRef = firebase.database().ref(`player/${playerId}`)
+
+      playerRef.set({
+        id: playerId,
+        name: "ZSOTYAX",
+        direction: "RIGHT",
+        color: "blue",
+        x: 3,
+        y: 3,
+        bullets: 0,
+      })
+      playerRef.onDisconnect().remove()
+      initGame();
+    }else{
+      
+    }
+  })
+  firebase.auth().signInAnonymously().catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode,errorMessage)
+  });
+
+})();
